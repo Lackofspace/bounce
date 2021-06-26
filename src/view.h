e62ac4f3439e201f98f2b9c227a63735109d3c6d
Lackofspace <86164563+Lackofspace@users.noreply.github.com>
2021-06-27 00:18:50 +0300

    Update view.h

--- src/view.h
+++ src/view.h
@@ -2,9 +2,9 @@
 
 using namespace sf;
 
-sf::View view;                                                    // объявили sfml объект "вид", который и является камерой
+sf::View view;                                                      // объявили sfml объект "вид", который и является камерой
 /// Функция SetCameraSize.
 void SetCameraSize() {
-    view.setCenter(200.f, 150.f);                                // центр камеры
-    view.setSize(400.f, 300.f);                                    // размер камеры
+    view.setCenter(200.f, 150.f);                                   // центр камеры
+    view.setSize(400.f, 300.f);                                     // размер камеры
 }
